75dce96048a840188a34e52ac1a7b2b87072cdc7
pavel demyanov <pavel@demjanov.ru>
2025-03-13 18:52:37 +0300

    Types или Interfaces?

--- app.ts
+++ app.ts
@@ -1,45 +1,18 @@
+/* interface User {
+    name: string
+}
+
 interface User {
-    name: string,
-    age: number,
-    skills: string[],
-
-    log: (id: number) => string
-};
-
-type User2 = {
-    name: string,
-    age: number,
-    skills: string[],
-
-    log: (id: number) => string
-};
-
-interface Role {
-    roleId: number
+    age: number
 }
 
-interface UserWithRole extends User, Role {
-    createdAt: Date;
+const user: User = {
+    name: 'john',
+    age: 25
+} */
+
+type ID = string | number;
+
+interface IDI {
+    ID: string | number;
 }
-
-let user: UserWithRole = {
-    name: "John", 
-    age: 30,
-    skills: ['1', '2'],
-    roleId: 123,
-    createdAt: new Date(),
-
-    log(id: number): string {
-        return '123';
-    }
-}
-
-interface UserDic {
-    [index: number]: User
-}
-
-type UserDic2 = {
-    [index: number]: User
-}
-
-type ud = Record<number, User>
