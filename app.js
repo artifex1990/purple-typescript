19bb2527309c057fe35b9aecdbd56b3563957e8d
pavel demyanov <pavel@demjanov.ru>
2025-03-14 21:57:10 +0300

    Особенности наследования

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 "use strict";
-class User {
-    set login(l) {
-        this._login = 'user-' + l;
+class Payment {
+    constructor(id) {
+        this.status = 'new';
+        this.id = id;
+    }
+    pay() {
+        this.status = 'paid';
     }
 }
-const user = new User();
-user.login = 'myLogin';
-console.log(user);
+class PersistedPayment extends Payment {
+    constructor() {
+        const id = Math.random();
+        super(id);
+    }
+    save() {
+    }
+    pay(date) {
+        super.pay();
+        if (date) {
+            this.paidAt = date;
+        }
+    }
+}
+new PersistedPayment();
+class User {
+    constructor() {
+        this.name = 'user';
+        console.log(this.name);
+    }
+}
+class Admin extends User {
+    constructor() {
+        super();
+        this.name = 'admin';
+        console.log(this.name);
+    }
+}
+new Admin();
