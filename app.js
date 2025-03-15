81799e9669cade51a97fc43d1b8a594731222f2c
pavel demyanov <pavel@demjanov.ru>
2025-03-15 13:58:34 +0300

    Упражнение - Делаем корзину товаров(учитель)

--- app.js
+++ app.js
@@ -3,100 +3,65 @@ class Cart {
     constructor() {
         this.products = [];
     }
-    hasProductToCart(id) {
-        return this.products.find(p => p.id === id) !== undefined;
-    }
     addProduct(product) {
-        if (this.hasProductToCart(product.id)) {
-            return;
-        }
         this.products.push(product);
     }
-    deleteProduct(id) {
-        this.products = this.products.filter(p => p.id !== id);
+    deleteProduct(productId) {
+        this.products = this.products.filter((p) => p.id !== productId);
     }
-    calculateTotal() {
-        return this.products.reduce((total, p) => total + p.price, 0);
+    getSum() {
+        return this.products
+            .map((p) => p.price)
+            .reduce((p1, p2) => p1 + p2, 0);
     }
-    set delivery(delivery) {
-        this._delivery = delivery;
+    setDelivery(delivery) {
+        this.delivery = delivery;
     }
-    get delivery() {
-        return this._delivery;
-    }
-    checkout() {
-        return this.delivery !== undefined && this.products.length !== 0;
+    checkOut() {
+        if (this.products.length === 0) {
+            throw new Error('Can\'t checkout. Products not found.');
+        }
+        if (!this.delivery) {
+            throw new Error('Can\'t checkout. Delivery not selected.');
+        }
+        return { success: true };
     }
 }
 class Product {
     constructor(id, title, price) {
-        this._id = id;
-        this._title = title;
-        this._price = price;
-    }
-    get id() {
-        return this._id;
-    }
-    get title() {
-        return this._title;
-    }
-    set title(title) {
-        this._title = title;
-    }
-    get price() {
-        return this._price;
-    }
-    set price(price) {
-        this._price = price;
+        this.id = id;
+        this.title = title;
+        this.price = price;
     }
 }
 class Delivery {
     constructor(date) {
-        this._date = date;
-    }
-    get date() {
-        return this._date;
-    }
-    set date(date) {
-        this._date = date;
+        this.date = date;
     }
 }
 class DeliveryHome extends Delivery {
     constructor(date, address) {
         super(date);
-        this._address = address;
-    }
-    get address() {
-        return this._address;
-    }
-    set address(address) {
-        this._address = address;
+        this.address = address;
     }
 }
-class DeliveryPvz extends Delivery {
-    constructor(idPvz) {
+class DeliveryShop extends Delivery {
+    constructor(shopId) {
         super(new Date());
-        this._idPvz = idPvz;
-    }
-    get idPvz() {
-        return this._idPvz;
-    }
-    set idPvz(idPvz) {
-        this._idPvz = idPvz;
+        this.shopId = shopId;
     }
 }
 const product1 = new Product(1, "Iphone", 1000);
 const product2 = new Product(2, "Samsung", 1200);
 const cart = new Cart();
 const deliveryHome = new DeliveryHome(new Date(), "Moscow");
-const deliveryPvz = new DeliveryPvz(1);
-console.log(cart.checkout());
+const deliveryPvz = new DeliveryShop(1);
 cart.addProduct(product1);
 cart.addProduct(product2);
 console.log(cart);
-console.log(cart.calculateTotal());
-cart.delivery = deliveryHome;
+console.log(cart.getSum());
+cart.setDelivery(deliveryHome);
 console.log(cart);
 cart.deleteProduct(2);
 console.log(cart);
-console.log(cart.checkout());
+console.log(cart.checkOut());
--- app.ts
+++ app.ts
@@ -1,120 +1,66 @@
+type DeliveryOptions  = DeliveryHome | DeliveryShop;
+
 class Cart {
     private products: Product[] = [];
-    private _delivery: Delivery;
+    private delivery: DeliveryOptions;
 
-    private hasProductToCart(id: number): boolean {
-        return this.products.find(p => p.id === id) !== undefined;
-    }
-
-    addProduct(product: Product): void {
-        if (this.hasProductToCart(product.id)) {
-            return;
-        }
-
+    public addProduct(product: Product): void {
         this.products.push(product);
     }
 
-    deleteProduct(id: number): void {
-        this.products = this.products.filter(p => p.id !== id);
+    public deleteProduct(productId: number): void {
+        this.products = this.products.filter((p: Product) => p.id !== productId);
     }
 
-    calculateTotal(): number {
-        return this.products.reduce((total, p) => total + p.price, 0);
+    public getSum(): number {
+        return this.products
+            .map((p: Product) => p.price)
+            .reduce((p1: number, p2: number) => p1 + p2, 0);
     }
 
-    set delivery(delivery: Delivery) {
-        this._delivery = delivery;
+    public setDelivery(delivery: DeliveryOptions) {
+        this.delivery = delivery;
     }
 
-    get delivery(): Delivery {
-        return this._delivery;
-    }
+    public checkOut() {
+        if (this.products.length === 0) {
+            throw new Error('Can\'t checkout. Products not found.');
+        }
 
-    checkout(): boolean {
-        return this.delivery !== undefined && this.products.length !== 0;
+        if (!this.delivery) {
+            throw new Error('Can\'t checkout. Delivery not selected.');
+        }
+
+        return { success: true };
     }
 }
 
 
 class Product {
-    private _id: number;
-    private _title: string;
-    private _price: number;
-
-    constructor(id: number, title: string, price: number) {
-        this._id = id;
-        this._title = title;
-        this._price = price;
-    }
-
-    get id(): number {
-        return this._id;
-    }
-
-    get title(): string {
-        return this._title;
-    }
-    
-    set title(title: string) {
-        this._title = title;
-    }
-
-    get price(): number {
-        return this._price;
-    }
-
-    set price(price: number) {
-        this._price = price;
-    }
+    constructor(
+        public id: number, 
+        public title: string, 
+        public price: number
+    ) { }
 }
 
 class Delivery {
-    protected _date: Date;
-
-    constructor(date: Date) {
-        this._date = date;
-    }
-
-    get date(): Date {
-        return this._date;
-    }
-
-    set date(date: Date) {
-        this._date = date;
-    }
+    constructor(
+        public date: Date
+    ) {}
 }
 
 class DeliveryHome extends Delivery {
     private _address: string;
 
-    constructor(date: Date, address: string) {
+    constructor(date: Date, public address: string) {
         super(date);
-        this._address = address;
-    }
-
-    get address(): string {
-        return this._address;
-    }
-
-    set address(address: string) {
-        this._address = address;
     }
 }
 
-class DeliveryPvz extends Delivery {
-    private _idPvz: number;
-
-    constructor(idPvz: number) {
+class DeliveryShop extends Delivery {
+    constructor(public shopId: number) {
         super(new Date());
-        this._idPvz = idPvz;
-    }
-
-    get idPvz(): number {
-        return this._idPvz;
-    }
-
-    set idPvz(idPvz: number) {
-        this._idPvz = idPvz;
     }
 }
 
@@ -124,16 +70,14 @@ const product2 = new Product(2, "Samsung", 1200);
 const cart = new Cart();
 
 const deliveryHome = new DeliveryHome(new Date(), "Moscow");
-const deliveryPvz = new DeliveryPvz(1);
-
-console.log(cart.checkout());
+const deliveryPvz = new DeliveryShop(1);
 
 cart.addProduct(product1);
 cart.addProduct(product2);
 console.log(cart);
-console.log(cart.calculateTotal());
-cart.delivery = deliveryHome;
+console.log(cart.getSum());
+cart.setDelivery(deliveryHome);
 console.log(cart);
 cart.deleteProduct(2);
 console.log(cart);
-console.log(cart.checkout());
+console.log(cart.checkOut());
