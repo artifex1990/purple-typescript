6fa03c9fff01d5d71711332bac243572ad5b31ba
pavel demyanov <pavel@demjanov.ru>
2025-03-15 15:20:44 +0300

    Типизация this

--- app.ts
+++ app.ts
@@ -1,32 +1,27 @@
-class Payment {
-    private date: Date = new Date();
+class UserBuilder {
+    name: string;
 
-    getDate(this: Payment) {
-        return this.date;
+    setName(name: string): this {
+        this.name = name;
+        return this;
     }
 
-    getDateArrow = () => {
-        return this.date;
-    }
-}
-
-
-const p = new Payment();
-
-const user = {
-    id: 1,
-    paymentDate: p.getDate.bind(p),
-    paymentDateArrow: p.getDateArrow
-}
-
-// console.log(p.getDate());
-// console.log(user.paymentDate());
-// console.log(user.paymentDateArrow());
-
-class PaymentPersistent extends Payment {
-    save() {
-        return this.getDateArrow();
+    isAdmin(): this is AdminBuilder {
+        return this instanceof AdminBuilder;
     }
 }
 
-console.log(new PaymentPersistent().save());
+class AdminBuilder extends UserBuilder {
+    roles: [];
+}
+
+const res = new UserBuilder().setName('John');
+const re1s = new AdminBuilder().setName('John');
+
+let user: UserBuilder | AdminBuilder = new UserBuilder();
+
+if (user.isAdmin()) {
+    console.log(user);
+} else {
+    console.log(user);
+}
